fd912f68b0faaa3ecd67f31e1d5457ef4745f348
Celia Bofio <celiabofio@gmail.com>
2021-04-28 16:03:32 +0000

    Ajouter le nom du projet dans une variable

--- index.php
+++ index.php
@@ -6,7 +6,20 @@ $app_nom ="Celiaight";
 <head>
 <title><?php echo $app_nom ?></title>
 <body>
-<p>Bienvenue sur <?php echo $app_nom ?></p></body>
+    <h1 class="en-tete">Inscription</h1>
+    <div class="contenu">
+    <form id="formulaire-inscription" method="post" action="">
+    <label for="inscription-email">E-mail</label>
+    <input type="email" mame="email" id="inscription-email" placeholder="Entrez votre email">
+    <br>
+    <label for="inscription-mot-passe">mot de passe</label>
+    <input type="password" mame="code-secret" id="inscription-mot-passe" placeholder="Entrez votre mot de passe">
+    <br>
+    <button type="submit">S'inscrire</button>
+    </form>
+    </div>
+
+</body>
 
 </head>
 </html>
